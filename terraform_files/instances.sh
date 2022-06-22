716170fb10a79ed11a0218326d2c82db49e7ae0d
HARRAOUNINE Samir <106996752+harraounine@users.noreply.github.com>
2022-06-22 19:29:39 +0100

    Update instances.sh

--- terraform_files/instances.sh
+++ terraform_files/instances.sh
@@ -4,4 +4,4 @@ sudo yum upgrade -y
 sudo yum install httpd -y 
 sudo systemctl start httpd 
 sudo systemctl enable httpd 
-sudo chmod 777 echo "welcome to the YASSIR site" > /var/www/html/index.html
+sudo chmod 777 echo "welcome to the site" > /var/www/html/index.html
